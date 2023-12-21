4f28064642ff1ac7a903aa96fc27823df8c8e070
edofoschi <150843578+edofoschi@users.noreply.github.com>
2023-12-21 10:28:41 +0100

    Add files via upload

--- exam03/ex1/repeat_alpha.c
+++ exam03/ex1/repeat_alpha.c
@@ -1,29 +1,30 @@
 #include <unistd.h>
 
-int ft_repeat(char c)
+int repeat_alpha(char c)
 {
 	int repeat;
-	if(c >= 'a' && c <= 'z')
+
+	if (c >= 'a' && c <= 'z')
 		repeat = c - 'a' + 1;
-	else if(c >= 'A' && c <= 'Z')
+	if (c >= 'A' && c <= 'Z')
 		repeat = c - 'A' + 1;
-	else
-		repeat = 1;
-	return(repeat);
+	return (repeat);
 }
 
 int main(int ac, char **av)
 {
 	int repeat;
+
 	if(ac == 2)
 	{
 		while(*av[1])
 		{
-			repeat = ft_repeat(*av[1]);
-			while(repeat--)
+			repeat = repeat_alpha(*av[1]);
+			while (repeat--)
 				write(1, av[1], 1);
 			av[1]++;
 		}
 	}
 	write(1, "\n", 1);
+	return(0);
 }
